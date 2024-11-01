8a8d9dbc7987ea3826c76ed33b67a7cff9b93b3d
Johan Watelet <johanwatelet@gmail.com>
2024-11-01 14:20:57 +0100

    Initial commit

---
+++ src/test/java/com/johanwatelet/gardeningapp/GardeningAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.johanwatelet.gardeningapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GardeningAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
